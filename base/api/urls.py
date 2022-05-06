4d8470b1cf1bbdf912676c66de3759a23e3b74e2
Mert Gökmen <mertgkmeen@gmail.com>
2022-05-06 18:32:45 +0300

    iletisim ilet animasyon

--- base/api/urls.py
+++ base/api/urls.py
@@ -4,18 +4,14 @@ from . import views
 
 urlpatterns = [
     #!FORUM CEVABI OTOMATİKLEŞTİRME 
-    path("forum/cevap/<int:pk>/begen/",views.ForumCevabiniBegenme,name="forum_cevabini_begen"),
-    path("forum/cevap/<int:pk>/begenme/",views.ForumCevabiniBegenmeme,name="forum_cevabini_begenme"),
-    path("forum/cevap/<int:pk>/begen/sayisi/",views.ForumCevapBegenmeSayi,name="forum_cevabini_begen_sayi"),
-    path("forum/cevap/<int:pk>/begenme/sayisi/",views.ForumCevapBegenmemeSayi,name="forum_cevabini_begenme_sayi"),
+    path("forum/cevap/<int:pk>/begen/",views.ForumCevabiniBegen,name="forum_cevabini_begen"),
+    path("forum/cevap/<int:pk>/begenme/",views.ForumCevabiniBegenme,name="forum_cevabini_begenme"),
     path("forum/cevap/<int:pk>/begen/renk/",views.ForumCevapBegenRenk,name="forum_cevabini_begen_renk"),
     path("forum/cevap/<int:pk>/begenme/renk/",views.ForumCevapBegenmeRenk,name="forum_cevabini_begenme_renk"),
     
     #!FORUM OTOMATİKLEŞTİRME 
-    path("forum/<int:pk>/begen/",views.ForumBegenme,name="forum__begen"),
-    path("forum/<int:pk>/begenme/",views.ForumBegenmeme,name="forum_begenme"),
-    path("forum/<int:pk>/begen/sayisi/",views.ForumBegenmeSayi,name="forum_begen_sayi"),
-    path("forum/<int:pk>/begenme/sayisi/",views.ForumBegenmemeSayi,name="forum_begenme_sayi"),
+    path("forum/<int:pk>/begen/",views.ForumBegen,name="forum__begen"),
+    path("forum/<int:pk>/begenme/",views.ForumBegenme,name="forum_begenme"),
     path("forum/<int:pk>/begen/renk/",views.ForumBegenRenk,name="forum_begen_renk"),
     path("forum/<int:pk>/begenme/renk/",views.ForumBegenmeRenk,name="forum_begenme_renk"),
     
--- base/api/views.py
+++ base/api/views.py
@@ -5,51 +5,49 @@ from base.models import *
 
 
 #!FORUM CEVABI FONKSİYONLARI
-@api_view(['GET'])
-def ForumCevapBegenmeSayi(request,pk):
-    cevap = ForumSoruCevap.objects.get(id=pk).likes.all().count()
-    return Response(cevap)
 
-@api_view(['GET'])
-def ForumCevapBegenmemeSayi(request,pk):
-    cevap = ForumSoruCevap.objects.get(id=pk).dislikes.all().count()
-    return Response(cevap)
+@api_view(['POST','GET'])
+def ForumCevabiniBegen(request,pk):
+    if request.method=="POST":
+        user = User.objects.get(username=request.data.get("username"))
+        cevap = ForumSoruCevap.objects.get(id=pk)
+        if user in cevap.likes.all():
+            cevap.likes.remove(user.id)
+        else:
+            cevap.likes.add(user.id)
+            cevap.dislikes.remove(user.id)
+    
+    begen_sayi = ForumSoruCevap.objects.get(id=pk).likes.all().count()
+
+    return Response(begen_sayi)
 
 @api_view(['POST','GET'])
 def ForumCevabiniBegenme(request,pk):
-    user = User.objects.get(username=request.data.get("username"))
-    cevap = ForumSoruCevap.objects.get(id=pk)
-    if user in cevap.likes.all():
-        cevap.likes.remove(user.id)
-    else:
-        cevap.likes.add(user.id)
-        cevap.dislikes.remove(user.id)
-    
-    begenme = 0
-    if user in cevap.likes.all():
-        begenme=1
-    elif user not in cevap.likes.all():
-        begenme=0 
+    if request.method=="POST":
+        user = User.objects.get(username=request.data.get("username"))
+        cevap = ForumSoruCevap.objects.get(id=pk)
+        if user in cevap.dislikes.all():
+            cevap.dislikes.remove(user.id)
+        else:
+            cevap.dislikes.add(user.id)
+            cevap.likes.remove(user.id)
 
-    return Response(begenme)
+    begenme_sayi = ForumSoruCevap.objects.get(id=pk).dislikes.all().count()
+    
+    return Response(begenme_sayi)
 
 @api_view(['POST','GET'])
-def ForumCevabiniBegenmeme(request,pk):
+def ForumCevapBegenRenk(request,pk):
     user = User.objects.get(username=request.data.get("username"))
     cevap = ForumSoruCevap.objects.get(id=pk)
-    if user in cevap.dislikes.all():
-        cevap.dislikes.remove(user.id)
-    else:
-        cevap.dislikes.add(user.id)
-        cevap.likes.remove(user.id)
 
-    begenmeme = 0
-    if user in cevap.dislikes.all():
-        begenmeme=1
-    elif user not in cevap.dislikes.all():
-        begenmeme=0 
+    begen = 0
+    if user in cevap.likes.all():
+        begen="begenildi"
+    else:
+        begen="begenildi isaretlenmedi"
     
-    return Response(begenmeme)
+    return Response(begen)
 
 @api_view(['POST','GET'])
 def ForumCevapBegenmeRenk(request,pk):
@@ -65,67 +63,37 @@ def ForumCevapBegenmeRenk(request,pk):
     return Response(begenme)
 
 
-@api_view(['POST','GET'])
-def ForumCevapBegenRenk(request,pk):
-    user = User.objects.get(username=request.data.get("username"))
-    cevap = ForumSoruCevap.objects.get(id=pk)
-
-    begen = 0
-    if user in cevap.likes.all():
-        begen="begenildi"
-    else:
-        begen="begenildi isaretlenmedi"
-    
-    return Response(begen)
-
-
 
 #!FORUM FONKSİYONLARI
-@api_view(['GET'])
-def ForumBegenmeSayi(request,pk):
-    cevap = ForumSoru.objects.get(id=pk).likes.all().count()
-    return Response(cevap)
+@api_view(['POST','GET'])
+def ForumBegen(request,pk):
+    if request.method=="POST":
+        user = User.objects.get(username=request.data.get("username"))
+        cevap = ForumSoru.objects.get(id=pk)
+        if user in cevap.likes.all():
+            cevap.likes.remove(user.id)
+        else:
+            cevap.likes.add(user.id)
+            cevap.dislikes.remove(user.id)
+    
+    forum_sayi = ForumSoru.objects.get(id=pk).likes.all().count()
 
-@api_view(['GET'])
-def ForumBegenmemeSayi(request,pk):
-    cevap = ForumSoru.objects.get(id=pk).dislikes.all().count()
-    return Response(cevap)
+    return Response(forum_sayi)
 
 @api_view(['POST','GET'])
 def ForumBegenme(request,pk):
-    user = User.objects.get(username=request.data.get("username"))
-    cevap = ForumSoru.objects.get(id=pk)
-    if user in cevap.likes.all():
-        cevap.likes.remove(user.id)
-    else:
-        cevap.likes.add(user.id)
-        cevap.dislikes.remove(user.id)
-    
-    begenme = 0
-    if user in cevap.likes.all():
-        begenme=1
-    elif user not in cevap.likes.all():
-        begenme=0 
+    if request.method=="POST":
+        user = User.objects.get(username=request.data.get("username"))
+        cevap = ForumSoru.objects.get(id=pk)
+        if user in cevap.dislikes.all():
+            cevap.dislikes.remove(user.id)
+        else:
+            cevap.dislikes.add(user.id)
+            cevap.likes.remove(user.id)
 
-    return Response(begenme)
+    forum_sayi = ForumSoru.objects.get(id=pk).dislikes.all().count()
 
-@api_view(['POST','GET'])
-def ForumBegenmeme(request,pk):
-    user = User.objects.get(username=request.data.get("username"))
-    cevap = ForumSoru.objects.get(id=pk)
-    if user in cevap.dislikes.all():
-        cevap.dislikes.remove(user.id)
-    else:
-        cevap.dislikes.add(user.id)
-        cevap.likes.remove(user.id)
-
-    begenmeme = 0
-    if user in cevap.dislikes.all():
-        begenmeme=1
-    elif user not in cevap.dislikes.all():
-        begenmeme=0 
-    
-    return Response(begenmeme)
+    return Response(forum_sayi)
 
 @api_view(['POST','GET'])
 def ForumBegenmeRenk(request,pk):
