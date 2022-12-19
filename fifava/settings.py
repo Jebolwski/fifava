11403eb65c13b19b13ff7ee946a36d21b40a9531
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-12-19 11:22:49 +0300

    Update settings.py

--- fifava/settings.py
+++ fifava/settings.py
@@ -158,11 +158,3 @@ EMAIL_HOST_PASSWORD = 'ujqxewmwvgriknem'
 
 # SECURE_SSL_REDIRECT = True 
 
-# AWS_ACCESS_KEY_ID = 'AKIA32ZGHYSFVKRESFPD'
-# AWS_SECRET_ACCESS_KEY = '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'
-# AWS_STORAGE_BUCKET_NAME = 'fifavox-s3-buckets-1'
-# AWS_S3_FILE_OVERWRITE = False
-# AWS_QUERYSTRING_AUTH = False
-# AWS_DEFAULT_ACL = None
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
